55d47f3fc27514f8e28ec773fe30a804e8de85e5
Bo Xiao <bxiao@flatironinstitute.org>
2024-01-23 23:24:30 -0500

    fix bugs in setting up the Kitaev Hamiltonian

--- 2d_kitaev_honeycomb.jl
+++ 2d_kitaev_honeycomb.jl
@@ -37,17 +37,17 @@ end
 
 let
   # Set up the lattice parameters
-  Nx_unit_cell = 4
-  Ny_unit_cell = 4
+  Nx_unit_cell = 3
+  Ny_unit_cell = 3
   Nx = 2 * Nx_unit_cell
   Ny = Ny_unit_cell
   N = Nx * Ny
 
   # Set up the interaction parameters for the Hamiltonian
   # Fine tunning these parameters can lead to different topological properties
-  Jx = 1.0
-  Jy = 1.0
-  Jz = 1.0
+  Jx = -1.0
+  Jy = -1.0
+  Jz = -1.0
 
   # honeycomb lattice
   # lattice = honeycomb_lattice_Cstyle(Nx, Ny; yperiodic=true)
@@ -57,22 +57,43 @@ let
   @show number_of_bonds
   @show lattice
   
+  # Construct the Hamiltonian as an MPO
+  # This part explicitly depends on how the lattice is mapped from a 2d honeycomb lattice to a 1d chain
   os = OpSum()
+  # count_bonds = 0
   for b in lattice
     tmp_x = div(b.s1 - 1, Ny) + 1
+    tmp_y = mod(b.s1 - 1, Ny) + 1
+    tmp_x_prime = div(b.s2 - 1, Ny) + 1
+    tmp_y_prime = mod(b.s2 - 1, Ny) + 1
+    
     if mod(tmp_x, 2) == 0
-      os .+= Jz, "Sz", b.s1, "Sz", b.s2
+      os .+= -Jz, "Sz", b.s1, "Sz", b.s2
+      # @show b.s1, b.s2
+      # count_bonds += 1
     else
-      if mod(b.s1, 2) == mod(b.s2, 2)
-        os .+= Jx, "Sx", b.s1, "Sx", b.s2
-      else
-        os .+= Jy, "Sy", b.s1, "Sy", b.s2
+      if (mod(b.s1, 2) == mod(b.s2, 2)) || (mod(b.s1, 2) != mod(b.s2, 2) && tmp_y == 1 && tmp_y_prime == Ny)
+        os .+= -Jx, "Sx", b.s1, "Sx", b.s2
+        # @show b.s1, b.s2
+        # count_bonds += 1
+      elseif mod(b.s1, 2) != mod(b.s2, 2) && abs(tmp_x - tmp_x_prime) == 1
+        os .+= -Jy, "Sy", b.s1, "Sy", b.s2
+        # @show b.s1, b.s2
+        # count_bonds += 1
       end 
     end
+
+    if tmp_x == 1 && tmp_x_prime == Nx
+      os .+= -Jz, "Sz", b.s2, "Sz", b.s1
+      # @show b.s2, b.s1
+      # count_bonds += 1
+    end
   end
+  # @show count_bonds
   sites = siteinds("S=1/2", N; conserve_qns=false)
   H = MPO(os, sites)
   
+
   # Initialize wavefunction to a random MPS
   # of bond-dimension 10 with same quantum
   # numbers as `state`
@@ -86,18 +107,19 @@ let
   cutoff  = [1E-8]
   # noise   = [1E-6, 1E-7, 1E-8, 0.0]
 
+  
   tmp_observer = energyObserver()
+  Sx₀ = expect(ψ₀, "Sx", sites = 1 : N)
+  # Sy₀ = expect(ψ₀, "Sy", sites = 1 : N)
   Sz₀ = expect(ψ₀, "Sz", sites = 1 : N)
   energy, ψ = dmrg(H, ψ₀; nsweeps, maxdim, cutoff, observer = tmp_observer)
   Sz = expect(ψ, "Sz", sites = 1 : N)
+  Sx = expect(ψ, "Sx", sites = 1 : N)
+  # Sy = expect(ψ, "Sy", sites = 1 : N)
+  xxcorr = correlation_matrix(ψ, "Sx", "Sx", sites = 1 : N)
+  # yycorr = correlation_matrix(ψ, "Sy", "Sy", sites = 1 : N)
   zzcorr = correlation_matrix(ψ, "Sz", "Sz", sites = 1 : N)
   
-  # Sz₀ = expect(ψ₀, "Sz", sites = 1 : N)
-  # # Sx₀ = expect(ψ₀, "Sx", sites = 1 : N)
-  # energy, ψ = dmrg(H, ψ₀; nsweeps, maxdim, cutoff)
-  # Sz = expect(ψ, "Sz", sites = 1 : N)
-  # # Sx = expect(ψ, "Sx", sites = 1 : N)
-  # zzcorr = correlation_matrix(ψ, "Sz", "Sz", sites = 1 : N)
 
   # Check the variance of the energy
   H2 = inner(H, ψ, H, ψ)
@@ -105,6 +127,7 @@ let
   variance = H2 - E₀^2
   @show variance
 
+  
   # # 1/22/2024
   # # Check von Neumann entanglement entropy per bond
   # SvN = entanglement_entropy_bonds(ψ, lattice)
@@ -116,18 +139,22 @@ let
   @show E₀
   @show tmp_observer.ehistory
   
-  h5open("data/2d_kitaev_honeycomb_lattice_pbc_rings_L$(Nx)W$(Ny).h5", "w") do file
+  h5open("data/2d_kitaev_honeycomb_lattice_pbc_rings_L$(Nx)W$(Ny)_AFM.h5", "w") do file
     write(file, "psi", ψ)
     write(file, "NormalizedE0", energy / number_of_bonds)
     write(file, "E0", energy)
     write(file, "E0variance", variance)
     # write(file, "Entropy", SvN)
+    write(file, "Sx0", Sx₀)
+    write(file, "Sx",  Sx)
+    write(file, "Cxx", xxcorr)
+    # write(file, "Sy0", Sy₀)
+    # write(file, "Sy", Sy)
+    # write(file, "Cyy", yycorr)
     write(file, "Sz0", Sz₀)
     write(file, "Sz",  Sz)
     write(file, "Czz", zzcorr)
     write(file, "Ehist", tmp_observer.ehistory)
-    # write(file, "Sx0", Sx₀)
-    # write(file, "Sx",  Sx)
   end
 
   return
