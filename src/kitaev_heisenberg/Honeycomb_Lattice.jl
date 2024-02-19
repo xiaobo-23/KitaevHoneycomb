c7ea889d90de5a02a9aba031d8988315954724e3
Bo Xiao <bxiao@flatironinstitute.org>
2024-02-19 18:05:08 -0500

    Add twisted boundary condition

--- src/kitaev_heisenberg/Honeycomb_Lattice.jl
+++ src/kitaev_heisenberg/Honeycomb_Lattice.jl
@@ -169,6 +169,58 @@ function honeycomb_lattice_rings_pbc(Nx::Int, Ny::Int; yperiodic=false)::Lattice
 end
 
 
+function honeycomb_lattice_rings_twist(Nx::Int, Ny::Int; yperiodic=false)::Lattice
+	"""
+	  Using the ring ordering scheme
+	  Nx needs to be an even number
+	"""
+	yperiodic = yperiodic && (Ny > 2)
+	N = Nx * Ny - 2
+	Nbond = trunc(Int, 3/2 * N) - 4 + (yperiodic ? 0 : trunc(Int, Nx / 2))
+	@show Nbond
+	
+	latt = Lattice(undef, Nbond)
+	b = 0
+	for n in 1:N
+		# Configure the x coordinate
+		if n < Ny
+			x = 1
+		else
+			x = div(n - Ny, Ny) + 2
+		end
+		# Configure the y coordinate
+		if n < Ny
+			y = n + 1
+		else
+			y = mod(n, Ny) + 1
+		end
+		@show n, x, y, b
+  
+	  	# horizontal bonds
+		if mod(x, 2) == 0 && x < Nx
+			latt[b += 1] = LatticeBond(n, n + Ny)
+		end
+  
+		# bonds to accomodate simga_x * sigma_x and sigma_y * sigma_y interactions
+		if Ny > 1
+			if mod(x, 2) == 1 
+				if y > 1
+					latt[b += 1] = LatticeBond(n, n + Ny - 1)
+					if n + Ny <= N
+						latt[b += 1] = LatticeBond(n, n + Ny)
+					end
+				else
+					latt[b += 1] = LatticeBond(n, n + Ny)
+					latt[b += 1] = LatticeBond(n, n - 1)
+				end
+			end
+		end
+	# @show latt
+	end
+	return latt
+end
+
+
 function honeycomb_lattice_Cstyle(Nx::Int, Ny::Int; yperiodic=false)::Lattice
   """
 	Using the C-style ordering scheme
@@ -190,7 +242,7 @@ function honeycomb_lattice_Cstyle(Nx::Int, Ny::Int; yperiodic=false)::Lattice
 		# x-direction bonds for A sublattice
 		if mod(x, 2) == 0 && x < Nx
 			latt[b += 1] = LatticeBond(n, n + 2 * Ny - 1)
-			@show n, x, y, latt[b]
+			@show n, x, y, b
 		end
 
 		# bonds for B sublattice
@@ -209,7 +261,6 @@ function honeycomb_lattice_Cstyle(Nx::Int, Ny::Int; yperiodic=false)::Lattice
 			end
 		end
 	end
-
 	# @show latt
 	return latt
 end
